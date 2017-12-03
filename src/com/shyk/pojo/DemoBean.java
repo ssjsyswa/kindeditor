24d0d61ef6e005f061f9cd228592b059e6e9452d
ssjsyswa <ssjsyswa@163.com>
2017-12-03 20:49:34 +0800

    玩

---
+++ src/com/shyk/pojo/DemoBean.java
@@ -0,0 +1,24 @@
+package com.shyk.pojo;
+
+public class DemoBean
+{
+	String id ;
+	String desc ;
+	public String getId()
+	{
+		return id;
+	}
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+	public String getDesc()
+	{
+		return desc;
+	}
+	public void setDesc(String desc)
+	{
+		this.desc = desc;
+	}
+	
+}
